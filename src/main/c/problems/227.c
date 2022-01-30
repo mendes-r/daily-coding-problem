ad5dd99ed451648199fd50c59e13679f220d7d97
Ricardo Mendes <mendes.r@gmx.com>
2022-01-30 19:22:38 +0000

    FIX bug in problem 227

--- src/main/c/problems/227.c
+++ src/main/c/problems/227.c
@@ -6,44 +6,71 @@
 // Do this in O(N) time.
 
 bool sumOfSubset();
+int gaussSum();
+int getMissingSum();
 
 int main(){
 
-    int sortedIntegers[] = {1, 2, 3, 10};
+    int sortedIntegers[] = {1, 2, 3, 5, 10, 11, 35};
     int size = sizeof sortedIntegers / sizeof sortedIntegers[0];
     int last = sortedIntegers[size - 1];
+
+    int missingSum = 0;
 
     int i, result = -1;
     int counter = 0;
 
+
+    bool flag = true;
         
-    for(i = 1; i < last; i ++)
+    for(i = 1; i < last && flag; i ++)
     {
-        if ((sortedIntegers[counter] > i)){
-            if (sumOfSubset(i, sortedIntegers[counter - 1]))
+        if ((sortedIntegers[counter] > i)){ 
+
+            if (sumOfSubset(i, sortedIntegers[counter - 1], missingSum))
             {
                 result = i;
-                break;
-            } else {
+                flag = false;
+            } else {    
                 counter --;
             }
+            
+        } else if ((counter > 0) && (sortedIntegers[counter] == i) && (sortedIntegers[counter - 1] != (i - 1))) 
+        {
+            missingSum += getMissingSum(sortedIntegers[counter - 1], sortedIntegers[counter]);
         }
         counter ++;
     }
 
     if(result > 0){
-        printf("Result: %d\n", i);
+        printf("Result: %d\n", result);
     } else {
-        printf("No possible result!\n");
+        printf("Result: %d\n", (sortedIntegers[last] + 1));
     }
 
 }
 
-bool sumOfSubset(int sum, int lastElementSubset){
+bool sumOfSubset(int sum, int lastElementSubset, int missingSum){
     // Gauss sum
-    int maxSubsetSum = lastElementSubset * (lastElementSubset + 1) / 2;
-    if (sum <= maxSubsetSum){
+    int maxSubsetSum = gaussSum(lastElementSubset) - missingSum;
+    if (sum <= (maxSubsetSum))
+    {
         return false;
     }
     return true;
+}
+
+int gaussSum(int number){
+    return number * (number + 1) / 2;
+}
+
+int getMissingSum (int start, int end){
+    int result = 0;
+    if (start == 0){return 0;};
+    while (start < (end - 1)) {
+        start ++;
+        result += start;        
+    }
+
+    return result;
 }
